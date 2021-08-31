529f16f5a9dde6bdd7ca2025a5325d694e440647
Shashank Tyagi <shashanktyagi725@gmail.com>
2021-08-31 16:38:27 +0530

    updated

--- DOUBLY_LINKED_LIST/DoublyLinkedList.java
+++ DOUBLY_LINKED_LIST/DoublyLinkedList.java
@@ -2,7 +2,10 @@ package DOUBLY_LINKED_LIST;
 
 public class DoublyLinkedList {
     Node head;
-    public void insert(int value)
+
+    int count=0;
+
+    public void insertAtEnd(int value)
     {
         Node dbn=new Node();
         dbn.previous=null;
@@ -12,28 +15,44 @@ public class DoublyLinkedList {
         if (head==null)
         {
             head=dbn;
+            count++;
         }
-        else
-        {
-            Node dbn2=head;
-            while(dbn2.next!=null)
-            {
-                Node prevVal=dbn2;
-                dbn2.previous=prevVal;
-                dbn2=dbn2.next;
+        else {
+            Node dbn2 = head;
+            while (dbn2.next != null) {
+                dbn2 = dbn2.next;
             }
-            dbn2.next=dbn;
+            dbn2.next = dbn;
+            dbn.previous = dbn2;
+
+            count++;
         }
+
+
+        System.out.println("Head of the Doubly linked list is:-"+ head.data);
+        System.out.println("Tail of the Doubly linked list is:-"+ dbn.data);
     }
 
     public void insertAtStart(int value)
     {
-    Node dbn=new Node();
-    dbn.previous=null;
-    dbn.data=value;
-    dbn.next=head;
-    head=dbn;
+        Node dbn=new Node();
+        if (head != null) {
+            dbn.previous = null;
+            dbn.data = value;
+            dbn.next = head;
+            head.previous=dbn;
+        }
+        head=dbn;
 
+        count++;
+
+        while(dbn.next!=null)
+        {
+            dbn=dbn.next;
+        }
+
+        System.out.println("Head of the Doubly linked list is:-"+ head.data);
+        System.out.println("Tail of the Doubly linked list is:-"+ dbn.data);
 
     }
 
@@ -44,10 +63,16 @@ public class DoublyLinkedList {
         dbn3.previous=null;
         dbn3.data=value;
         dbn3.next=null;
+
          if (index==0)
          {
          insertAtStart(value);
           }
+
+         else if(index==count-1)
+         {
+             insertAtEnd(value);
+         }
 
          else
          {
@@ -59,42 +84,104 @@ public class DoublyLinkedList {
              dbn3.next=storage.next;
              storage.next=dbn3;
 
+             count++;
          }
-    }
-
-public void delete(int index)
-{
-    if (index==0)
-    {
-        head=head.next;
-        head.previous=null;
-    }
-    else
-    {
-        Node h=head;
-        Node change_prev=null; //to store the node value of the element before the element to be deleted
-        Node change_next=null; // to store the the node value of the element after the element to be deleted
-        while(h.next!=null) {
-            for (int i = 0; i < index - 1; i++) {
-                h = h.next;
-            }
-            change_next = h.next;  //
-            change_prev = change_next.next;
-            h.next = change_prev;
-            change_prev.previous = h;
+        while(dbn3.next!=null)
+        {
+            dbn3=dbn3.next;
         }
 
+        System.out.println("Head of the Doubly linked list is:-"+ head.data);
+        System.out.println("Tail of the Doubly linked list is:-"+ dbn3.data);
+    }
+
+
+
+
+public void deleteFromStart()
+
+    {
+        Node h = head;
+        head = h.next;
+        h.previous = null;
+
+        count--;
+
+        Node temp=head;
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+        }
+
+        System.out.println("After Removing node from start");
+        show();
+        System.out.println("Head of the Doubly linked list is:-"+ head.data);
+        System.out.println("Tail of the Doubly linked list is:-"+ temp.data);
 
     }
 
-}
+    public void deleteFromEnd() {
+        Node h1 = head;
+        while (h1.next != null) {
+            h1 = h1.next;
+        }
+        Node last = h1.previous;
+        last.next = null;
+
+        count--;
+
+        System.out.println("After Removing node from the end");
+        show();
+        System.out.println("Head of the Doubly linked list is:-" + head.data);
+        System.out.println("Tail of the Doubly linked list is:-" + last.data);
+    }
+
+
+    public void deleteFromIndex(int index)
+    {
+        if (index==0)
+        {
+            deleteFromStart();
+        }
+
+ else {
+            Node h1=head;
+            for (int i = 0; i < index - 1; i++) {
+                h1 = h1.next;
+            }
+
+            Node temp_nxt= h1.next;
+            h1.next = temp_nxt.next;
+
+            Node temp_prev=temp_nxt.next;
+            temp_prev.previous=h1;
+
+            count--;
+
+            Node temp=head;
+            while(temp.next!=null)
+            {
+                temp=temp.next;
+            }
+
+            System.out.println("After Removing node from the end");
+            show();
+            System.out.println("Head of the Doubly linked list is:-" + head.data);
+            System.out.println("Tail of the Doubly linked list is:-" + temp.data);
+
+        }
+//
+    }
+
+
+
     public void show() {
      Node nd= head;
         while (nd.next != null) {
             System.out.print(nd.data+" ");
             nd= nd.next;
         }
-        System.out.print(nd.data); //VIMP: printing of last element of linked list
+        System.out.println(nd.data); //VIMP: printing of last element of linked list
     }
 
 }
--- DOUBLY_LINKED_LIST/Runner.java
+++ DOUBLY_LINKED_LIST/Runner.java
@@ -4,33 +4,32 @@ import DOUBLY_LINKED_LIST.DoublyLinkedList;
 
 public class Runner {
 
-    public static void main(String[] args) throws NullPointerException
-    {
-    DoublyLinkedList d_list=new DoublyLinkedList();
-         d_list.insert(100);
-        d_list.insert(200);
-        d_list.insert(300);
-        d_list.insert(400);
+    public static void main(String[] args) throws NullPointerException {
+        DoublyLinkedList d_list = new DoublyLinkedList();
+        d_list.insertAtEnd(100);
+        d_list.insertAtEnd(200);
 
-        d_list.insertAtStart(1);
-        d_list.insertAtStart(5);
-        d_list.insertAtStart(7);
+        d_list.insertAtStart(500);
+        d_list.insertAtStart(600);
 
-        d_list.insert(999);
+        d_list.insertAtEnd(800);
 
-        d_list.insertAtIndex(8,79);
-        d_list.insertAtIndex(9,89);
-        d_list.insertAtIndex(10,99);
+        d_list.insertAtIndex(5,999);
+        d_list.insertAtIndex(2,300);
+        d_list.insertAtIndex(4,400);
 
-
-//we use try catch block as it will throw a null pointer exception when we are using the last index element removal
-try {
-    d_list.delete(10);
-}
-catch(Exception e)
-{
-    System.out.println("The exception is:"+ e);
-}
         d_list.show();
+
+             d_list.deleteFromIndex(0);
+             d_list.deleteFromIndex(4);
+
+
+        d_list.deleteFromStart();
+        d_list.deleteFromStart();
+
+
+        d_list.deleteFromEnd();
+        d_list.deleteFromEnd();
+
     }
 }
